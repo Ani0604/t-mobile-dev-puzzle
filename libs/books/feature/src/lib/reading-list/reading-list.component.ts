9b3f5619eff6ec47f8ad462fb066c1986f0feb24
Anirudh <shanthan@Shans-MBP.lan>
2024-02-07 10:42:36 -0500

    Task 3: Undo actions

--- libs/books/feature/src/lib/reading-list/reading-list.component.ts
+++ libs/books/feature/src/lib/reading-list/reading-list.component.ts
@@ -1,18 +1,25 @@
 import { Component } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
 import { Store } from '@ngrx/store';
-import { getReadingList, removeFromReadingList } from '@tmo/books/data-access';
+import { addToReadingList, getReadingList, removeFromReadingList } from '@tmo/books/data-access';
 
 @Component({
   selector: 'tmo-reading-list',
   templateUrl: './reading-list.component.html',
-  styleUrls: ['./reading-list.component.scss']
+  styleUrls: ['./reading-list.component.scss'],
 })
 export class ReadingListComponent {
   readingList$ = this.store.select(getReadingList);
 
-  constructor(private readonly store: Store) {}
+  constructor(private readonly store: Store, private _snackBar: MatSnackBar) {}
 
   removeFromReadingList(item) {
     this.store.dispatch(removeFromReadingList({ item }));
+
+    let snackBar = this._snackBar.open('Book removed from reading list', 'Undo', item);
+    snackBar.onAction().subscribe(() => {
+      let book = { ...item, id: item.bookId };
+      this.store.dispatch(addToReadingList({ book }));
+    });
   }
 }
